309fd927369f60956e2842714a6d34e6c616a9b9
Max <62596612+TentationNwaar@users.noreply.github.com>
2024-10-04 21:04:03 +0200

    finalisation

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -10,45 +11,57 @@ int main() {
     bool est_Premier;
     char relancer_programme;
 
-    cout << "Ce programme ..." << endl;
-
     do {
-        cout << "\nentrer une valeur [2-1000] : ";
-        cin >> limite_utilisateur;
-    } while (limite_utilisateur < 2 || limite_utilisateur > 1000);
+        cout << "Ce programme ...\n" << endl;
 
-    cout << "\nVoici la liste des nombres premiers" << endl;
+        do {
+            cout << "entrer une valeur [2-1000] : ";
+            cin >> limite_utilisateur;
+        } while (limite_utilisateur < 2 || limite_utilisateur > 1000);
 
-    //Calcul, divisible par 1 et lui-même uniquement
-    for (int i = 2; i <= limite_utilisateur; ++i) {
-        est_Premier = true;
-        for (int diviseur = 2; diviseur < i; ++diviseur) {
-            if (i % diviseur == 0) {
-                est_Premier = false;
-                break;
+        cout << "\nVoici la liste des nombres premiers" << endl;
+
+        //Calcul, divisible par 1 et lui-même uniquement
+        for (int i = 2; i <= limite_utilisateur; ++i) {
+            est_Premier = true;
+            for (int diviseur = 2; diviseur < i; ++diviseur) {
+                if (i % diviseur == 0) {
+                    est_Premier = false;
+                    break;
+                }
+            }
+
+            //Afficher nbr premier sur 5 colonnes
+            if (est_Premier) {
+                cout << setw(4) << i; //Ajustement proposé par chatGPT
+                valeur_affichee++;
+
+                if (valeur_affichee == valeur_affichage_max) {
+                    cout << '\n';
+                    valeur_affichee = 0;
+                }
             }
         }
 
-        // Afficher nbr premier sur 5 colonnes
-        // Solution proposée par ChatGPT
-        // Ma solution faisait disparaître une valeur
-        // (réinitialisation du compteur à 0)
-        if (est_Premier) {
-            cout << '\t' << i;
-            valeur_affichee++;
-        }
-            if (valeur_affichee % valeur_affichage_max == 0){
-              cout << '\n';
-            }
+        //Ajustement de l'affichage pour les grandes valeurs
+        if (valeur_affichee != 0) {
+            cout << '\n';
         }
 
-    //Demander si l'utilisateur veut recommencer
-    do {
-        cout << "\n\nVoulez-vous recommencer [O/N] : ";
-        cin >> relancer_programme;
-    } while (relancer_programme != 'O' && relancer_programme != 'N'); //Vérifier que les caractères soient justes
+        do {
+            cout << "\nVoulez-vous recommencer [O/N] : ";
+            cin >> relancer_programme;
+        } while (relancer_programme != 'O' && relancer_programme != 'N');
 
-    cout << "\nFin de programme" << endl;
+        if (relancer_programme == 'O'){
+            continue;
+        }
+
+        else{
+            cout << "\nFin de programme" << endl;
+        }
+
+    } while (relancer_programme != 'N');
 
     return EXIT_SUCCESS;
 }
