a868b75e915ff1eebffd8bfe5e95d33e5a10ae65
Max <62596612+TentationNwaar@users.noreply.github.com>
2024-10-04 19:57:38 +0200

    Ajustement de l'affichage

--- main.cpp
+++ main.cpp
@@ -4,38 +4,47 @@ using namespace std;
 
 int main() {
 
+    const int valeur_affichage_max = 5;
+    int valeur_affichee = 0;
     int limite_utilisateur;
-    bool estPremier;
+    bool est_Premier;
     char relancer_programme;
 
-    cout << "Ce programme affiche les nombres premiers suivant la saisie" << endl;
+    cout << "Ce programme ..." << endl;
 
     do {
-        cout << "Entrez une valeur [2-1000] : ";
+        cout << "\nentrer une valeur [2-1000] : ";
         cin >> limite_utilisateur;
     } while (limite_utilisateur < 2 || limite_utilisateur > 1000);
 
+    cout << "\nVoici la liste des nombres premiers" << endl;
+
     //Calcul, divisible par 1 et lui-même uniquement
     for (int i = 2; i <= limite_utilisateur; ++i) {
-        estPremier = true;
+        est_Premier = true;
         for (int diviseur = 2; diviseur < i; ++diviseur) {
             if (i % diviseur == 0) {
-                estPremier = false;
+                est_Premier = false;
                 break;
             }
         }
 
-        if (estPremier){
-            cout << i << endl;
+        // Afficher nbr premier sur 5 colonnes
+        // Solution proposée par ChatGPT
+        // Ma solution faisait disparaître une valeur
+        // (réinitialisation du compteur à 0)
+        if (est_Premier) {
+            cout << '\t' << i;
+            valeur_affichee++;
         }
-
-    }
-
-    //Afficher nbr premier sur 5 colonnes
+            if (valeur_affichee % valeur_affichage_max == 0){
+              cout << '\n';
+            }
+        }
 
     //Demander si l'utilisateur veut recommencer
     do {
-        cout << "Voulez-vous recommencer [O/N] : ";
+        cout << "\n\nVoulez-vous recommencer [O/N] : ";
         cin >> relancer_programme;
     } while (relancer_programme != 'O' && relancer_programme != 'N'); //Vérifier que les caractères soient justes
 
